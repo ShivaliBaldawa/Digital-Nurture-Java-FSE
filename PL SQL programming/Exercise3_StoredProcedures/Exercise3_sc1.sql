7266f79f3f4f4cba4859a363e0da433575927b83
ShivaliBaldawa <shivalibaldawa15@gmail.com>
2026-06-29 10:29:19 +0530

    ex3

---
+++ PL SQL programming/Exercise3_StoredProcedures/Exercise3_sc1.sql
@@ -0,0 +1,23 @@
+CREATE OR REPLACE PROCEDURE ProcessMonthlyInterest
+IS
+BEGIN
+    UPDATE Accounts
+    SET Balance = Balance + (Balance * 0.01)
+    WHERE AccountType = 'Savings';
+
+    COMMIT;
+
+    DBMS_OUTPUT.PUT_LINE('Monthly interest processed successfully.');
+END;
+/
+
+BEGIN
+    ProcessMonthlyInterest;
+END;
+/
+
+SELECT AccountID,
+       AccountType,
+       Balance
+FROM Accounts;
+
